e922394380ca20ba61caf0fb97fe2d60a45ac797
bitwise <31191954+buttwize@users.noreply.github.com>
2017-09-20 15:32:17 -0700

    add utils module

---
+++ lib/combatless/utils.ex
@@ -0,0 +1,25 @@
+defmodule Combatless.Utils do
+  @moduledoc false
+
+
+  @spec format_integer(integer) :: string
+  def format_integer(integer) do
+    integer
+    |> Integer.to_charlist()
+    |> Enum.reverse()
+    |> partition_integer([])
+    |> to_string()
+  end
+
+  defp partition_integer([a, b, c, ?- | _tail], acc) do
+    [?-, c, b, a | acc]
+  end
+
+  defp partition_integer([a, b, c, d | tail], acc) do
+    partition_integer [d | tail], [?,, c, b, a | acc]
+  end
+
+  defp partition_integer(rest, acc) do
+    Enum.reverse(rest) ++ acc
+  end
+end
