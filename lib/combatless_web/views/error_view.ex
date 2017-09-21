284c9eeb305a3a66d4cfe06cc39bf6c5889046ee
bitwise <31191954+buttwize@users.noreply.github.com>
2017-09-21 14:05:08 -0700

    make error pages actual html files instead of pure strings

--- lib/combatless_web/views/error_view.ex
+++ lib/combatless_web/views/error_view.ex
@@ -1,16 +1,6 @@
 defmodule CombatlessWeb.ErrorView do
   use CombatlessWeb, :view
 
-  def render("404.html", assigns) do
-    render(__MODULE__, "404.html", assigns)
-  end
-
-  def render("500.html", assigns) do
-    render(__MODULE__, "500.html", assigns)
-  end
-
-  # In case no render clause matches or no
-  # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
     render "500.html", assigns
   end
