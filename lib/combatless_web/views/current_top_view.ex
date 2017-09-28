26150f741a19c646387fa477cc4ede3a969d129a
bitwise <31191954+buttwize@users.noreply.github.com>
2017-09-28 13:45:11 -0700

    move current tops table creation to a function

--- lib/combatless_web/views/current_top_view.ex
+++ lib/combatless_web/views/current_top_view.ex
@@ -11,6 +11,7 @@ defmodule CombatlessWeb.CurrentTopView do
                     construction)
 
   @default_sizes [25, 50, 100, 200]
+  @period_order [:day, :week, :month, :year]
 
   def allowed_skills(), do: @allowed_skills
   def active_class(current_skill, page_skill) do
@@ -30,7 +31,21 @@ defmodule CombatlessWeb.CurrentTopView do
     end
   end
 
-  def build_current_top_list(conn, %Scrivener.Page{} = page, skill) do
+  def build_current_top_tables(conn, current_tops, skill) do
+    for period <- @period_order do
+      period_name = Atom.to_string(period)
+      content_tag(:div, id: period_name, class: "col d-flex flex-column align-items-center hiscore-ranks") do
+        [
+          content_tag(:h3, String.capitalize(period_name)),
+          content_tag(:table, class: "table table-hover table-sm") do
+            content_tag(:tbody, build_current_top_list(conn, current_tops[period], period, skill))
+          end
+        ]
+      end
+    end
+  end
+
+  def build_current_top_list(conn, %Scrivener.Page{} = page, period, skill) do
     base_index = (page.page_size * (page.page_number - 1)) + 1
     for {current, rank} <- Enum.with_index(page.entries, base_index) do
       name = current.account.name
@@ -39,7 +54,7 @@ defmodule CombatlessWeb.CurrentTopView do
           content_tag(:td, "#{rank}.", class: "data"),
           content_tag(
             :td,
-            content_tag(:a, printable_account_name(name), href: profile_path(conn, :show, name)),
+            content_tag(:a, printable_account_name(name), href: profile_path(conn, :show, name, period: period)),
             class: "hiscore-name"
           ),
           content_tag(:td, Utils.delimit(current.value), class: "data"),
