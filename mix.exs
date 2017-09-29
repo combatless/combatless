869a3bf8248723eddafbdc882aea315840db5085
bitwise <31191954+buttwize@users.noreply.github.com>
2017-09-29 05:29:26 -0700

    bump 0.12.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Combatless.Mixfile do
   def project do
     [
       app: :combatless,
-      version: "0.12.1",
+      version: "0.12.3",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
