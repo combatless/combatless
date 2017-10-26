46846dae8e86b84cd9710e1a3c01f38938322a98
bitwise <31191954+buttwize@users.noreply.github.com>
2017-10-26 00:25:55 -0700

    significantly speed up current_top fetching (over 10x less time)

--- lib/combatless/current_top/current_tops.ex
+++ lib/combatless/current_top/current_tops.ex
@@ -4,6 +4,8 @@ defmodule Combatless.CurrentTops do
   import Combatless.Utils, only: [period_to_arbitrary_days: 1]
   alias Combatless.Accounts
   alias Combatless.Datapoints.Datapoint
+  alias Combatless.Datapoints.SkillDatapoint
+  alias Combatless.Datapoints.Skill
 
   def current_top(skill, period) do
     starting_time = Timex.shift(Timex.now(), period_to_arbitrary_days(period))
@@ -25,12 +27,14 @@ defmodule Combatless.CurrentTops do
   def current_top_query(skill), do: current_top_query(skill, Timex.shift(Timex.now(), days: -7))
   def current_top_query(skill, starting_time) do
     real_skill = if skill == "ehp", do: "overall", else: skill
+
     from(
-      d in Datapoint,
-      join: sd in assoc(d, :skill_datapoints),
-      join: s in assoc(sd, :skill),
-      group_by: d.account_id,
-      where: d.fetched_at > ^starting_time and s.slug == ^real_skill and d.is_valid == true,
+      d in subquery(unique_min_and_max_datapoints(starting_time)),
+      join: max in SkillDatapoint,
+      join: min in SkillDatapoint,
+      join: s in Skill,
+      on: s.slug == ^real_skill and max.skill_id == s.id and min.skill_id == s.id,
+      on: max.datapoint_id == d.max_id and min.datapoint_id == d.min_id,
       select: %{
         account_id: d.account_id
       }
@@ -39,16 +43,29 @@ defmodule Combatless.CurrentTops do
   end
 
   defp select_skill_value_fragment(query, "ehp") do
-    from [d, sd] in query,
+    from [d, max, min] in query,
          select_merge: %{
-           value: fragment("? - ?", max(sd.ehp), min(sd.ehp))
+           value: fragment("? - ?", max.ehp, min.ehp)
          }
   end
 
   defp select_skill_value_fragment(query, skill) do
-    from [d, sd] in query,
+    from [d, max, min] in query,
          select_merge: %{
-           value: fragment("? - ?", max(sd.xp), min(sd.xp))
+           value: fragment("? - ?", max.xp, min.xp)
          }
+  end
+
+  def unique_min_and_max_datapoints(starting_time) do
+    from(
+      d in Datapoint,
+      group_by: d.account_id,
+      where: d.fetched_at > ^starting_time and d.is_valid == true,
+      select: %{
+        account_id: d.account_id,
+        max_id: max(d.id),
+        min_id: min(d.id)
+      }
+    )
   end
 end
